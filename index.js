839b44d34aca9742b6e5087222b828d341fa1000
boconnor1993 <brendan_oconnor1993@hotmail.com>
2023-09-29 08:12:22 +1000

    Converted to class structure
    Converted existing object models to class based structure, ensuring
    full encapsulation.

--- index.js
+++ index.js
@@ -2,99 +2,116 @@
 const myLibrary = [];
 const divInsertElement = document.querySelector('#card-container')
 
-// Create Book object to be used for cards
-function Book(title, author, numPages, bookRead) {
-    this.title = title;
-    this.author = author;
-    this.numPages = numPages;
-    this.bookRead = bookRead;
+// Create Book class to be used for cards
+class Book {
+    constructor(title, author, numPages, bookRead) {
+        this.title = title;
+        this.author = author;
+        this.numPages = numPages;
+        this.bookRead = bookRead;
+        this.bookReadString = this.bookRead ? 'Book read' : 'Not read yet';
+    }
 
-    this.bookReadString = this.bookRead ? 'Book read' : 'Not read yet';
-
-    this.info = function() {
+    info() {
         return `${this.title} by ${this.author}, ${this.numPages} pages, ${this.bookReadString}`;
     }
+
+    toggleReadStatus(index) {
+        this.bookRead = !this.bookRead;  // Toggle read status
+        this.bookReadString = this.bookRead ? 'Book read' : 'Not read yet';  // Update read string
+    }
 }
 
-// Create a new Book object and save it to the index
-function addBookToLibrary(title, author, numPages, bookRead) {
-    const newBook = new Book(title, author, numPages, bookRead);
-    myLibrary.push(newBook);
-}
-
-// Create the cards and add them to the html
-function displayMyLibrary(myLibrary) {
-    // Clear existing cards
-    while (divInsertElement.firstChild) {
-        divInsertElement.removeChild(divInsertElement.firstChild);
+// Create Library class to be used on collection of books
+class Library {
+    constructor() {
+        this.books = [];
     }
 
-    myLibrary.forEach((book, index) => {
-        // Create the card container and give it a class of book
-        const bookDiv = document.createElement('div');
-        bookDiv.classList.add('book');
+    // Create a new Book object and save it to the index
+    addBookToLibrary(title, author, numPages, bookRead) {
+        const newBook = new Book(title, author, numPages, bookRead);
+        this.books.push(newBook);
+    }
 
-        // Add in the elements to the card
-        // Add title
-        const titlePara = document.createElement('p');
-        titlePara.textContent = book.title;
-        titlePara.classList.add('title');
-        bookDiv.appendChild(titlePara);
+    // Create the cards and add them to the html
+    displayMyLibrary() {
+        // Clear existing cards
+        while (divInsertElement.firstChild) {
+            divInsertElement.removeChild(divInsertElement.firstChild);
+        }
 
-        // Add detail container
-        const detailContainer = document.createElement('div');
-        detailContainer.classList.add('detail-container')
-        // Add author
-        const authorPara = document.createElement('p');
-        authorPara.textContent = book.author;
-        detailContainer.appendChild(authorPara);
+        this.books.forEach((book, index) => {
+            // Create the card container and give it a class of book
+            const bookDiv = document.createElement('div');
+            bookDiv.classList.add('book');
 
-        // Add numPages
-        const numPagesPara = document.createElement('p');
-        numPagesPara.textContent = `${book.numPages} pages`;
-        detailContainer.appendChild(numPagesPara);
+            // Add in the elements to the card
+            // Add title
+            const titlePara = document.createElement('p');
+            titlePara.textContent = book.title;
+            titlePara.classList.add('title');
+            bookDiv.appendChild(titlePara);
 
-        // Add bookRead
-        const bookReadPara = document.createElement('p');
-        bookReadPara.textContent = book.bookReadString;
-        detailContainer.appendChild(bookReadPara);
+            // Add detail container
+            const detailContainer = document.createElement('div');
+            detailContainer.classList.add('detail-container')
+            // Add author
+            const authorPara = document.createElement('p');
+            authorPara.textContent = book.author;
+            detailContainer.appendChild(authorPara);
 
-        bookDiv.appendChild(detailContainer);
+            // Add numPages
+            const numPagesPara = document.createElement('p');
+            numPagesPara.textContent = `${book.numPages} pages`;
+            detailContainer.appendChild(numPagesPara);
 
-        // Add icons into a container
-        const iconContainer = document.createElement('div');
-        iconContainer.classList.add('icon-container');
-        // Add readIcon
-        const readIcon = document.createElement('i');
-        readIcon.className = 'bx bxs-book-add';
-        readIcon.addEventListener('click', function() {
-            toggleReadStatus(index);
+            // Add bookRead
+            const bookReadPara = document.createElement('p');
+            bookReadPara.textContent = book.bookReadString;
+            detailContainer.appendChild(bookReadPara);
+
+            bookDiv.appendChild(detailContainer);
+
+            // Add icons into a container
+            const iconContainer = document.createElement('div');
+            iconContainer.classList.add('icon-container');
+            // Add readIcon
+            const readIcon = document.createElement('i');
+            readIcon.className = 'bx bxs-book-add';
+            readIcon.addEventListener('click', () => {
+                myLibraryInstance.toggleReadStatus(index);
+            });
+            iconContainer.appendChild(readIcon);
+            //Add trashIcon
+            const trashIcon = document.createElement('i');
+            trashIcon.className = 'bx bxs-trash';
+            trashIcon.addEventListener('click', () => {
+                myLibraryInstance.deleteBook(index);
+            });
+            
+            iconContainer.appendChild(trashIcon);
+            bookDiv.appendChild(iconContainer);
+
+            // Append the card to the container
+            divInsertElement.appendChild(bookDiv);
         });
-        iconContainer.appendChild(readIcon);
-        //Add trashIcon
-        const trashIcon = document.createElement('i');
-        trashIcon.className = 'bx bxs-trash';
-        trashIcon.addEventListener('click', function(){
-            deleteBook(index)
-        });
-        iconContainer.appendChild(trashIcon);
-        bookDiv.appendChild(iconContainer);
+    }   
 
-        // Append the card to the container
-        divInsertElement.appendChild(bookDiv);
-    });
+    toggleReadStatus(index) {
+        this.books[index].toggleReadStatus();
+        this.displayMyLibrary();
+    }
+    
+
+    deleteBook(index) {
+        this.books.splice(index, 1);
+        this.displayMyLibrary();
+    }
 }
 
-function toggleReadStatus(index) {
-    myLibrary[index].bookRead = !myLibrary[index].bookRead;  // Toggle read status
-    myLibrary[index].bookReadString = myLibrary[index].bookRead ? 'Book read' : 'Not read yet';  // Update read string
-    displayMyLibrary(myLibrary);  // Re-render the library
-}
-
-function deleteBook(index) {
-    myLibrary.splice(index, 1);
-    displayMyLibrary(myLibrary);
-}
+// Create an instance of the Library class
+const myLibraryInstance = new Library();
 
 // Modal logic to make it dynamic
 // Get important items from DOM
@@ -140,25 +157,24 @@ bookForm.addEventListener('submit', function(event) {
         const bookRead = readTrue ? true : false;
 
         // Add book to library, then clear form data and display cards
-        addBookToLibrary(title, author, numPages, bookRead);
+        // Call methods on the Library instance
+        myLibraryInstance.addBookToLibrary(title, author, numPages, bookRead);
         bookForm.reset();
         modal.style.display = 'none';
-        while (divInsertElement.firstChild) {
-            divInsertElement.removeChild(divInsertElement.firstChild);
-        }
-        displayMyLibrary(myLibrary);
+        myLibraryInstance.displayMyLibrary();
     }
     else {
         alert('Please complete all fields')
     }
 })
 
+
 // Add some popular books as library dummy data
-addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, true);
-addBookToLibrary("1984", "George Orwell", 328, false);
-addBookToLibrary("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 309, true);
-addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180, true);
-addBookToLibrary("Moby-Dick", "Herman Melville", 720, false);
+myLibraryInstance.addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, true);
+myLibraryInstance.addBookToLibrary("1984", "George Orwell", 328, false);
+myLibraryInstance.addBookToLibrary("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 309, true);
+myLibraryInstance.addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180, true);
+myLibraryInstance.addBookToLibrary("Moby-Dick", "Herman Melville", 720, false);
 
 // Display the library
-displayMyLibrary(myLibrary);
+myLibraryInstance.displayMyLibrary();
